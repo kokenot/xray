e052513ffa291275d01893053456a9f4b687a5e7
kokenot <johnmalay5518@gmail.com>
2022-09-29 00:36:33 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -3,8 +3,7 @@
 # stb nanopi
 mv /usr/bin/xray /usr/bin/xray-bak
 wget "https://github.com/solomonricky/Xray-core/releases/download/v1.6.0-1/Xray-linux-arm64-v8a.zip"
-unzip *.zip
+unzip Xray-linux-arm64-v8a.zip
 mv xray /usr/bin
 chmod +x /usr/bin/xray
-# rm *.zip *.dat LICENSE README.md
 xray version
