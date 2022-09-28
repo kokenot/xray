0c4f8ea9e89ebca9909a507dc5d4dce8e1e5e78a
kokenot <johnmalay5518@gmail.com>
2022-09-29 00:22:16 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # command update core xray armv8
+# stb nanopi
 mv /usr/bin/xray /usr/bin/xray-bak
-wget https://github.com/solomonricky/Xray-core/releases/download/v1.6.0-1/Xray-linux-arm64-v8a.zip
+wget "https://github.com/solomonricky/Xray-core/releases/download/v1.6.0-1/Xray-linux-arm64-v8a.zip"
 unzip *.zip
 mv xray /usr/bin
 chmod +x /usr/bin/xray
