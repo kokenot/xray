eccdb09fa68591ff7cb9aab310348754c50cb2fd
kokenot <johnmalay5518@gmail.com>
2022-09-29 00:04:56 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-mv /usr/bin/xray /usr/bin/xray-bak && wget https://github.com/solomonricky/Xray-core/releases/download/v1.6.0-1/Xray-linux-arm64-v8a.zip && unzip *.zip && mv xray /usr/bin && chmod +x /usr/bin/xray && rm *.zip *.dat LICENSE README.md && xray version
+mv /usr/bin/xray /usr/bin/xray-bak
+wget https://github.com/solomonricky/Xray-core/releases/download/v1.6.0-1/Xray-linux-arm64-v8a.zip
+unzip *.zip
+mv xray /usr/bin
+chmod +x /usr/bin/xray
+rm *.zip *.dat LICENSE README.md
+xray version
