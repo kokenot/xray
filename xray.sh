dac531c4ea84c264289a0b48f603785b79c47128
kokenot <johnmalay5518@gmail.com>
2022-09-29 12:14:01 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # command update core xray armv8
 
-mv "/usr/bin/xray "/usr/bin/xray-bak" \
-&& wget "https://github.com/solomonricky/Xray-core/releases/download/v1.6.0-1/Xray-linux-arm64-v8a.zip" \
-&& unzip "Xray-linux-arm64-v8a.zip" \
-&& mv xray "/usr/bin" \
-&& chmod +x "/usr/bin/xray" \
-&& xray version
+rm -rf /usr/bin/xray && wget https://github.com/solomonricky/Xray-core/releases/download/v1.6.0-1/Xray-linux-arm64-v8a.zip && unzip *.zip && mv xray /usr/bin && chmod +x /usr/bin/xray && rm *.zip *.dat LICENSE README.md && xray version
