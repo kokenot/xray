dd456e1f223724d8fdec946cc1abc9df44c8118f
kokenot <johnmalay5518@gmail.com>
2022-09-29 15:59:09 +0800

    Add files via upload

---
+++ xrayArmv8.sh
@@ -0,0 +1,1 @@
+mv /usr/bin/xray /usr/bin/xray-bak && wget https://github.com/solomonricky/Xray-core/releases/download/v1.6.0-1/Xray-linux-arm64-v8a.zip && unzip *.zip && mv xray /usr/bin && chmod +x /usr/bin/xray && rm *.zip *.dat LICENSE README.md && xray version
