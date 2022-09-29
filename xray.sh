6aef9c35df29424223ec47efe1ed90f9f5c80a16
kokenot <johnmalay5518@gmail.com>
2022-09-29 15:58:02 +0800

    Delete xray.sh

--- xray.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# command update core xray armv8
-
-rm -rf /usr/bin/xray && wget https://github.com/solomonricky/Xray-core/releases/download/v1.6.0-1/Xray-linux-arm64-v8a.zip && unzip *.zip && mv xray /usr/bin && chmod +x /usr/bin/xray && rm *.zip *.dat LICENSE README.md && xray version
