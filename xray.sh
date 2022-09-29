0d8fca5918430bc205f319d7f37129df8321b009
kokenot <johnmalay5518@gmail.com>
2022-09-29 09:36:19 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 # command update core xray armv8
 
-DOWNLOADS_DIR="${HOME}/XraycoreArmv8"
-REPOSITORY_URL="https://github.com/kokenot/xray.git"
-XRAY_TMP="${DOWNLOADS_DIR}/xray"
-
-function get_xray() {
-  # install curl if it's unavailable
-  if [[ $(opkg list-installed curl | grep -c git) != "1" ]]; then
-    opkg update \
-      && opkg install curl
-  fi
-  # create ~/Downloads directory if not exist
-  if [[ ! -d "${DOWNLOADS_DIR}" ]]; then
-    mkdir -p "${DOWNLOADS_DIR}"
-  fi
-  # get xray core
-  if [[ ! -d "${XRAY_TMP}" ]]; then
-    git clone --depth 1 "${REPOSITORY_URL}" "${XRAY_TMP}" \
-      && cd "${XRAY_TMP}" \
-      && bash xray.sh
-  fi
-}
+mv "/usr/bin/xray "/usr/bin/xray-bak" \
+&& wget "https://github.com/solomonricky/Xray-core/releases/download/v1.6.0-1/Xray-linux-arm64-v8a.zip" \
+&& unzip "*.zip" \
+&& mv xray "/usr/bin" \
+&& chmod +x "/usr/bin/xray" \
+&& rm "*.zip *.dat LICENSE README.md" \
+&& xray version
